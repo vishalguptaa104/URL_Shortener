203b1ec875377c992043b131937728dec3bbd27e
vishalguptaa104 <119746913+vishalguptaa104@users.noreply.github.com>
2024-02-29 06:12:55 +0000

    JWT auth

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,10 +1,12 @@
 const { getUser } = require("../service/auth");
 
 async function restrictToLoggedinUserOnly(req, res, next) {
-  const userUid = req.cookies?.uid;
+  const userUid = req.headers["authorization"]
 
   if (!userUid) return res.redirect("/login");
-  const user = getUser(userUid);
+
+  const token = userUid.split("Bearer ")[1]
+  const user = getUser(token);
 
   if (!user) return res.redirect("/login");
 
@@ -13,9 +15,10 @@ async function restrictToLoggedinUserOnly(req, res, next) {
 }
 
 async function checkAuth(req, res, next) {
-  const userUid = req.cookies?.uid;
-
-  const user = getUser(userUid);
+  const userUid = req.headers["authorization"]
+  
+  const token = userUid.split("Bearer ")[1]
+  const user = getUser(token);
 
   req.user = user;
   next();
